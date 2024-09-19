2e7e222e2cf9367b0d10a0ab95049d5020ffc800
𝐀𝐛𝐡𝐢𝐬𝐡𝐞𝐤 𝐏𝐚𝐭𝐞𝐥 <abhishekpatel4220@gmail.com>
2024-09-19 14:32:38 +0000

    html and js

---
+++ script.js
@@ -0,0 +1,21 @@
+const ele = document.querySelector("img");
+const bot = document.querySelector("button");
+console.log(ele);
+
+function getrandom() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function getrandomUrl() {
+  const numWords = ["one", "two", "three", "four", "five", "six"];
+  const rn = getrandom() - 1;
+  console.log(numWords[rn]);
+
+  const url = `assets/images/dice-${numWords[rn]}.png`;
+  return url;
+}
+// ele.src = getrandomUrl();
+
+bot.addEventListener("click", function () {
+  ele.src = getrandomUrl();
+});
